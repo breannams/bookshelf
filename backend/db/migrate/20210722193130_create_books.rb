d0364402b38513954fdc0e8d030dbb75c723a56a
breannams <breannamarie_s@hotmail.com>
2021-07-22 15:32:13 -0400

    Add scaffolding for books

---
+++ backend/db/migrate/20210722193130_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.integer :pages
+      t.integer :genre_id
+
+      t.timestamps
+    end
+  end
+end
