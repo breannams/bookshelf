42b509c088b549fbec463a9c25e97ec5ac085832
breannams <breannamarie_s@hotmail.com>
2021-08-01 21:53:55 -0400

    Create methods to render Genres.

--- frontend/src/genreService.js
+++ frontend/src/genreService.js
@@ -7,6 +7,23 @@ class GenreService{
         .then(genreService.renderGenres) //data set gets passed into renderGenres implicitly
     }
     
-    
+    renderGenres(genres){
+        let genresArr = genres.data
+        genresArr.forEach(genre=>{
+            genreService.renderGenre(genre)
+        })
+    }
+
+    renderGenre(genre){
+       // debugger
+         let genreDetails = genre.attributes
+     
+         let genresContainer = document.getElementById("genres-container")
+         genresContainer.innerHTML += 
+       `
+       <strong>${genreDetails.name} | </strong>
+       `
+
+}
 
 }
